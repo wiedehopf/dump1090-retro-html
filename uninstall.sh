e850cac5d8cc4cda7e238ba1098969d442f35fd6
Matthias Wirth <matthias.wirth@gmail.com>
2019-06-28 16:18:22 +0000

    typo

--- uninstall.sh
+++ uninstall.sh
@@ -4,8 +4,8 @@ ipath=/usr/local/share/dump1090-retro-html
 
 rm -rf $ipath
 
-rm /etc/lighttpd/conv-enabled/88-dump1090-retro-html.conf
-rm /etc/lighttpd/conv-available/88-dump1090-retro-html.conf
+rm /etc/lighttpd/conf-enabled/88-dump1090-retro-html.conf
+rm /etc/lighttpd/conf-available/88-dump1090-retro-html.conf
 
 systemctl restart lighttpd
 
